602dc1865ab297b3dbe7a086c801bcb9f52a8097
malsenan <alsenanmr@gmail.com>
2026-03-14 21:41:48 -0700

    commit name test and progress

--- models.py
+++ models.py
@@ -2,8 +2,19 @@ from typing import TypedDict
 
 
 class BankTransaction(TypedDict):
-    account: str
     date: str
+    account: str
     description: str
     amount: float
     balance: float
+
+class FidelityTransaction(TypedDict):
+    date: str
+    account: str
+    symbol: str
+    description: str
+    quantity: float
+    price: float 
+    beginning_value: float
+    ending_value: float
+    cost_basis: float
